ae2c9fc644e0775a90bee07d8a2e03d6c0c4ebc3
Guilherme-Alves-Ferreira <guilherme.ferreira@bandtec.com.br>
2020-11-28 17:15:38 -0300

    a

--- src/main/java/configBanco/Conexao.java
+++ src/main/java/configBanco/Conexao.java
@@ -4,6 +4,6 @@ public class Conexao {
     // Conexão com o banco
     // Mude o server name, username, e password com as suas credenciais
     public String connectionUrl
-            = "jdbc:mysql://54.84.10.225:3306;"
+            = "jdbc:mysql://172.17.0.2:3306;"
             + "database=bancoSupervisor;";
 }
